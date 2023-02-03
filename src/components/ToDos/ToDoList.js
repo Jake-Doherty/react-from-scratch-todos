5a5e031f7c3e3bf55364a2629765fde004215875
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-02 22:27:38 -0800

    reworks component to add handleChange and map through todos

--- src/components/ToDos/ToDoList.js
+++ src/components/ToDos/ToDoList.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import { useContext } from 'react';
+import { toggleToDo } from '../../services/todos';
+import { ToDosContext } from '../../context/ToDosContext.js';
+import Todo from './Todo.js';
 
 export default function ToDoList() {
-  return <div>ToDoList</div>;
+  const { todos, setToDos } = useContext(ToDosContext);
+  if (!todos) return null;
+
+  const handleChange = async ({ id, completed }) => {
+    try {
+      const updatedToDo = await toggleToDo({ id, completed });
+      setToDos((prevToDos) =>
+        prevToDos.map((prevToDo) => (prevToDo.id === id ? updatedToDo : prevToDo))
+      );
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  return (
+    <>
+      {todos.map(({ id, description, completed }) => {
+        return <Todo key={id} {...{ id, description, completed, handleChange }} />;
+      })}
+    </>
+  );
 }
