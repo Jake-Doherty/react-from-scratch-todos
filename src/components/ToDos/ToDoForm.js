d163fa2bca28b6c18c20623a4db28a2a651c2896
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-02 18:49:37 -0800

    creates todo form

---
+++ src/components/ToDos/ToDoForm.js
@@ -0,0 +1,33 @@
+import { useContext, useState } from 'react';
+import { createToDoItem } from '../../services/todos';
+import { ToDosContext } from '../../context/ToDosContext.js';
+
+export default function ToDoForm() {
+  const [description, setDescription] = useState('');
+  const [completed, setCompleted] = useState(false);
+  const { setToDos } = useContext(ToDosContext);
+  const handleNewTodo = async () => {
+    try {
+      const todo = await createToDoItem(description, completed);
+      setToDos((prev) => [...prev, todo]);
+      setDescription('');
+      setCompleted(false);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+  return (
+    <div className="field is-grouped m-2">
+      <input
+        className="input m-2"
+        type="text"
+        placeholder="new item"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button className="button is-primary m-2" onClick={handleNewTodo}>
+        Add
+      </button>
+    </div>
+  );
+}
