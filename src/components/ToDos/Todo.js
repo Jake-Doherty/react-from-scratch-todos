8531d6e0a765d2b31ffe9286d4800f1ae6e01e3e
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-02 22:24:04 -0800

    reworks todo item to include data and handler

--- src/components/ToDos/Todo.js
+++ src/components/ToDos/Todo.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Todo() {
-  return <div>Todo</div>;
+export default function Todo({ id, description, completed, handleChange }) {
+  return (
+    <div id={`todo-${id}`}>
+      <label className="checkbox">
+        <span>ToDone</span>
+        <input
+          className="m-1"
+          type="checkbox"
+          checked={completed}
+          onChange={() => handleChange({ id, description, completed })}
+        />
+      </label>
+      <span className="description">{description}</span>
+      button
+    </div>
+  );
 }
