f781fcab1c78654aa665895da042862772ef2a69
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-02 23:29:52 -0800

    creates handleDelete and filters out deleted todo from todo state

--- src/components/ToDos/ToDoList.js
+++ src/components/ToDos/ToDoList.js
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
-import { toggleToDo } from '../../services/todos';
+import { toggleToDo, deleteToDo } from '../../services/todos';
 import { ToDosContext } from '../../context/ToDosContext.js';
 import Todo from './Todo.js';
 
 export default function ToDoList() {
   const { todos, setToDos } = useContext(ToDosContext);
+
   if (!todos) return null;
 
   const handleChange = async ({ id, completed }) => {
@@ -18,10 +19,22 @@ export default function ToDoList() {
     }
   };
 
+  const handleDelete = async ({ id }) => {
+    try {
+      await deleteToDo({ id });
+
+      const filteredTodos = (todos) => todos.filter((todo) => todo.id !== id);
+
+      setToDos(filteredTodos(todos));
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
   return (
     <>
       {todos.map(({ id, description, completed }) => {
-        return <Todo key={id} {...{ id, description, completed, handleChange }} />;
+        return <Todo key={id} {...{ id, description, completed, handleChange, handleDelete }} />;
       })}
     </>
   );
