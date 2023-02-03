d6bcb47267c787b82fea03e8aec80a5c40d344f0
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-02 22:22:49 -0800

    adds useLocation to load todos when path is redirected

--- src/context/ToDosContext.js
+++ src/context/ToDosContext.js
@@ -1,22 +1,31 @@
 import { createContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { getToDos } from '../services/todos';
 
 const ToDosContext = createContext();
 
 const ToDosProvider = ({ children }) => {
+  const location = useLocation();
   const [todos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    const fetchToDos = async () => {
-      try {
-        const data = await getToDos();
-        setToDos(data);
-      } catch (e) {
-        console.error(e.message);
-      }
-    };
-    fetchToDos();
-  }, []);
-  return <ToDosContext.Provider value={{ todos, setToDos }}>{children}</ToDosContext.Provider>;
+    if (location.pathname === '/todos') {
+      const fetchToDos = async () => {
+        try {
+          setLoading(true);
+          const data = await getToDos();
+          setToDos(data);
+        } catch (e) {
+          console.error(e.message);
+        }
+      };
+      fetchToDos();
+    }
+    setLoading(false);
+  }, [location.pathname]);
+  return (
+    <ToDosContext.Provider value={{ todos, setToDos, loading }}>{children}</ToDosContext.Provider>
+  );
 };
 
 export { ToDosContext, ToDosProvider };
