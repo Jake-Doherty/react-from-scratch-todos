e05b4e1626e5dabc2c4aad6798b85a4b6dfc5543
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-02-02 18:40:34 -0800

    adds header w signout and redirects to paths based on user sign in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth.js';
+import Header from './components/Header/Header.js';
 import Todos from './components/ToDos/Todos.js';
+import { useUser } from './context/UserContext.js';
 
 function App() {
+  const { user } = useUser();
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/todos" component={Todos} />
+        <Route exact path="/">
+          <>
+            {user && <Redirect to="/todos" />}
+            {!user && <Redirect to="/auth/sign-in" />}
+          </>
+        </Route>
       </Switch>
     </div>
   );
